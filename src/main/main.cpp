daa6f660bf4a217bbb377c61982f616dd64d71d0
15446-rus75 <vladabramov06@yandex.ru>
2026-05-11 02:46:56 +0300

    app finally can run...

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,21 +1,23 @@
 #include <QApplication>
+
 #include "../controller/controller.hpp"
 #include "../storage/memorystorage.hpp"
-//#include "../view/taskplannerview.hpp"
-#include "taskplanner.hpp"
+#include "../view/iview.hpp"
+#include "../view/taskplannerview.hpp"
 
 int main(int argc, char *argv[])
 {
-  QApplication a(argc, argv);
-//  storage::MemoryStorage storage;
-//  controller::Controller controller;
- // view::TaskPlannerView view;
+  QApplication app(argc, argv);
 
- // controller.setStorage(&storage);
- // controller.setView(&view);
-  //view.show();
-  //controller.start();
-  TaskPlanner w;
-  w.show();
-  return QCoreApplication::exec();
+  storage::MemoryStorage storage;
+  controller::Controller controller;
+  view::TaskPlannerView view;
+
+  controller.setStorage(&storage);
+  controller.setView(&view);
+
+  view.show();
+  controller.start();
+
+  return QApplication::exec();
 }
