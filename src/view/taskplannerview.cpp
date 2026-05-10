066b6f8af5138fb26b2a588879ccdd9e09a4170c
15446-rus75 <vladabramov06@yandex.ru>
2026-05-11 01:54:08 +0300

    add private slots of TaskPlannerView

--- src/view/taskplannerview.cpp
+++ src/view/taskplannerview.cpp
@@ -78,3 +78,105 @@ void view::TaskPlannerView::updateStats(int total, int completed, int today)
   ui->labelStatsCompleted->setText("✅ Выполнено: " + QString::number(completed));
   ui->labelStatsToday->setText("📌 На сегодня: " + QString::number(today));
 }
+
+void view::TaskPlannerView::onCalendarClicked(const QDate &date)
+{
+  emit dateSelected(date);
+}
+
+void view::TaskPlannerView::onSearchTextChanged(const QString &text)
+{
+  emit filterChanged(storage::Filter::Search, QVariant(text));
+}
+
+void view::TaskPlannerView::onFilterStateChanged(int state)
+{
+  Q_UNUSED(state)
+  if (ui->checkBoxAll->isChecked())
+  {
+    emit filterChanged(storage::Filter::ShowAll, QVariant());
+  }
+  else if (ui->checkBoxToday->isChecked())
+  {
+    emit filterChanged(storage::Filter::ShowToday, QVariant());
+  }
+  else if (ui->checkBoxOverdue->isChecked())
+  {
+    emit filterChanged(storage::Filter::ShowOverdue, QVariant());
+  }
+}
+
+void view::TaskPlannerView::onPriorityIndexChanged(int index)
+{
+  const storage::Priority priority = indexToPriority(index);
+  emit filterChanged(storage::Filter::Priority, QVariant::fromValue(priority));
+}
+
+void view::TaskPlannerView::onAddClicked()
+{
+  showTaskCreationForm();
+}
+
+void view::TaskPlannerView::onEditClicked()
+{
+  const int taskId = getSelectedTaskId();
+  if (taskId != -1)
+  {
+    emit taskEditRequested(taskId);
+  }
+}
+
+void view::TaskPlannerView::onDeleteClicked()
+{
+  const int taskId = getSelectedTaskId();
+  if (taskId != -1)
+  {
+    emit taskDeleteRequested(taskId);
+  }
+}
+
+void view::TaskPlannerView::onMarkCompleteClicked()
+{
+  const int taskId = getSelectedTaskId();
+  if (taskId != -1)
+  {
+    emit taskCompleteRequested(taskId);
+  }
+}
+
+void view::TaskPlannerView::onSortClicked()
+{
+  if (m_currentSortCriterion == storage::Criterion::Date)
+  {
+    m_currentSortCriterion = storage::Criterion::Priority;
+  }
+  else if (m_currentSortCriterion == storage::Criterion::Priority)
+  {
+    m_currentSortCriterion = storage::Criterion::Completed;
+  }
+  else
+  {
+    m_currentSortCriterion = storage::Criterion::Date;
+  }
+  emit sortRequested(m_currentSortCriterion);
+}
+
+void view::TaskPlannerView::onFormSaveClicked()
+{
+  storage::Task task = formToTask();
+  if (m_currentTaskId == -1)
+  {
+    emit taskAddRequested(task);
+  }
+  else
+  {
+    task.id = m_currentTaskId;
+    emit taskUpdateRequested(task);
+  }
+  closeTaskCreationForm();
+}
+
+void view::TaskPlannerView::onFormCancelClicked()
+{
+  closeTaskCreationForm();
+}
