513eb69d58bf0e196d463b56c6f9787d75ce8713
MrMels625 <MrMels625@gmail.com>
2026-06-21 21:25:46 +0300

    dummy virtual method impls added in order to make tests work :)

--- src/storage/memorystorage.hpp
+++ src/storage/memorystorage.hpp
@@ -21,6 +21,24 @@ namespace storage
     QList< Task > getTasksFiltered(const QString &searchText, bool today, bool overdue, Priority priority) const override;
     QList< Task > getSortedTasks(const QList< Task > &tasks, Criterion criterion) const noexcept override;
 
+    // Заглушки функционала геймификации из IStorage
+    UserProgress getUserProgress() const override;
+    void updateUserProgress(const UserProgress &progress) override;
+    void addXP(int amount, const QString &reason) override;
+    void updateStreak(const QDate &currentDate) override;
+    int getCurrentLevel() const override;
+    int getTotalXP() const override;
+    int getStreakDays() const override;
+
+    QList< Achievement > getAllAchievements() const override;
+    void unlockAchievement(const QString &achievementId) override;
+    bool isAchievementUnlocked(const QString &achievementId) const override;
+    Achievement getAchievementById(const QString &id) const override;
+
+    QList< QString > getUnlockedLocations() const override;
+    void unlockLocation(const QString &locationId) override;
+    bool isLocationUnlocked(const QString &locationId) const override;
+
   private:
     QList< Task > tasks_;
     int nextId_;
@@ -28,7 +46,12 @@ namespace storage
     void saveToFile() noexcept override;
     void loadFromFile() noexcept override;
     void load() noexcept;
+
+    // Заглушки приватных методов геймификации из IStorage
+    void saveGamificationData() noexcept override;
+    void loadGamificationData() noexcept override;
   };
 }
 
 #endif
+
