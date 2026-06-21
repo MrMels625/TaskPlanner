d7ed6cea98e89651edf99e4615b2aecc1739f1e5
MrMels625 <MrMels625@gmail.com>
2026-06-21 20:12:57 +0300

    main for tests added

---
+++ test/storage/main_test.cpp
@@ -0,0 +1,25 @@
+#define BOOST_TEST_MODULE StorageTests
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#include <QCoreApplication>
+
+struct GlobalQtFixture
+{
+  GlobalQtFixture()
+  {
+    static int   argc = 1;
+    static char  name[] = "run_tests";
+    static char *argv[] = {name, nullptr};
+    app_ = new QCoreApplication(argc, argv);
+  }
+
+  ~GlobalQtFixture()
+  {
+    delete app_;
+  }
+
+private:
+  QCoreApplication *app_;
+};
+
+BOOST_GLOBAL_FIXTURE(GlobalQtFixture);
