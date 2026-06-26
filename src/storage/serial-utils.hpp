6103b986a5e431ff52a3a7e5fe8d688b3aca8b73
MrMels625 <MrMels625@gmail.com>
2026-06-26 23:05:27 +0300

    serialization decls for gamification added

--- src/storage/serial-utils.hpp
+++ src/storage/serial-utils.hpp
@@ -3,6 +3,7 @@
 #include <QJsonObject>
 #include <QString>
 #include "task.hpp"
+#include "igamification_storage.hpp"
 
 namespace storage
 {
@@ -10,11 +11,22 @@ namespace storage
   {
     inline constexpr auto k_fileName = "tasks.json";
     inline constexpr auto k_fileNameBackup = "tasks.backup.json";
+    inline constexpr auto k_gamificationFileName = "gamification.json";
+    inline constexpr auto k_gamificationFileNameBackup = "gamification.backup.json";
 
     QString filePath(const QString &name);
+
     QJsonObject taskToJson(const Task &task);
     Task taskFromJson(const QJsonObject &obj);
     bool tryLoad(const QString &path, QList< Task > &tasks, int &nextId);
+
+    QJsonObject progressToJson(const UserProgress &progress);
+    UserProgress progressFromJson(const QJsonObject &obj);
+
+    QJsonObject achievementToJson(const Achievement &achievement);
+    Achievement achievementFromJson(const QJsonObject &obj);
+
+    bool tryLoadGamification(const QString &path, UserProgress &progress, QList< Achievement > &achievements);
   }
 }
 
